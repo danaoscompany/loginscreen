97c7248c0da8310fdcdf75e1a089475d4ef00f47
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-06-14 22:46:12 +0700

    Last commit

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'https://login.oprekmania.com',
+	'hostname' => 'login.oprekmania.com',
 	'username' => 'oprekma1_loginscr',
 	'password' => 'HelloWorld@123',
 	'database' => 'oprekma1_apps',

f2dd5c6cbd7e8779f9841b4bc28bc4eb62704af2
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-06-14 13:00:00 +0700

    Last commit

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -2,6 +2,24 @@
 
 class Admin extends CI_Controller {
 
+	public function login() {
+		$phone = $this->input->post('phone');
+		$password = $this->input->post('password');
+		$expiry = $this->input->post('expiry');
+		$users = $this->db->query("SELECT * FROM `user` WHERE `phone`='" . $phone . "' AND `password`='" . $password . "'")->result_array();
+		if (sizeof($users) > 0) {
+			$user = $users[0];
+			echo json_encode(array(
+				'response_code' => 1,
+				'user_id' => intval($user['id'])
+			));
+		} else {
+			echo json_encode(array(
+				'response_code' => -2
+			));
+		}
+	}
+
 	public function get_users() {
 		$start = intval($this->input->post('start'));
 		$length = intval($this->input->post('length'));

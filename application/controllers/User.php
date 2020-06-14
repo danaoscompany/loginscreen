3740925d1f3df3695bc26cdbd5772c6bb4dee5c5
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-06-14 07:24:21 +0700

    first commit

---
+++ application/controllers/User.php
@@ -0,0 +1,60 @@
+<?php
+
+class User extends CI_Controller {
+
+	public function login_with_google() {
+		$phone = $this->input->post('phone');
+		$googleID = $this->input->post('google_id');
+		$androidID = $this->input->post('android_id');
+		$expiry = $this->input->post('expiry');
+		$users = $this->db->query("SELECT * FROM `user` WHERE `google_id`='" . $googleID . "'")->result_array();
+		if (sizeof($users) > 0) {
+			$user = $users[0];
+			$userID = intval($user['id']);
+			$this->db->query("UPDATE `user` SET `android_id`='" . $androidID . "', `phone`='" . $phone . "', `expiry`='" . $expiry . "' WHERE `id`=" . $userID);
+			echo json_encode(array(
+				'response_code' => 1,
+				'user_id' => $userID,
+				'profile_completed' => intval($user['profile_completed']),
+				'user' => $user
+			));
+		} else {
+			$this->db->query("INSERT INTO `user` (`android_id`, `google_id`, `phone`, `expiry`) VALUES ('" . $androidID . "', '" . $googleID . "', '" . $phone . "', '" . $expiry . "')");
+			$userID = intval($this->db->insert_id());
+			echo json_encode(array(
+				'response_code' => -1,
+				'user_id' => $userID
+			));
+		}
+	}
+	
+	public function complete_data() {
+		$userID = intval($this->input->post('id'));
+		$firstName = $this->input->post('first_name');
+		$lastName = $this->input->post('last_name');
+		$email = $this->input->post('email');
+		$this->db->where('id', $userID);
+		$this->db->update('user', array(
+			'first_name' => $firstName,
+			'last_name' => $lastName,
+			'email' => $email,
+			'profile_completed' => 1
+		));
+	}
+	
+	public function is_profile_completed() {
+		$userID = intval($this->input->post('id'));
+		$users = $this->db->query("SELECT * FROM `user` WHERE `id`=" . $userID)->result_array();
+		if (sizeof($users) > 0) {
+			$user = $users[0];
+			echo json_encode(array(
+				'profile_completed' => intval($user['profile_completed']),
+				'user' => $user
+			));
+		} else {
+			echo json_encode(array(
+				'profile_completed' => 0
+			));
+		}
+	}
+}

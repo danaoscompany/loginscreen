5e45b53e816e29ba6e2f9d1268efec1f26a072ce
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-06-15 05:24:21 +0700

    Last commit

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -44,7 +44,6 @@ class User extends CI_Controller {
 				'response_code' => -2
 			));
 		}
-		header("Cache-Control: no-store, no-cache, must-revalidate,\"public, no-transform, must-revalidate\"");
 	}
 
 	public function login_with_google() {

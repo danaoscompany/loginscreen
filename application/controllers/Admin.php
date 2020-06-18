b038504be55b894d7e9cec659d172bfd772aa33a
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-06-18 11:54:13 +0700

    Last commit

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -33,9 +33,10 @@ class Admin extends CI_Controller {
 	}
 
 	public function get_users() {
+		$adminID = intval($this->input->post('admin_id'));
 		$start = intval($this->input->post('start'));
 		$length = intval($this->input->post('length'));
-		$users = $this->db->query("SELECT * FROM `user` ORDER BY `first_name` ASC LIMIT " . $start . "," . $length)->result_array();
+		$users = $this->db->query("SELECT * FROM `user` WHERE `admin_id`=" . $adminID . " ORDER BY `first_name` ASC LIMIT " . $start . "," . $length)->result_array();
 		for ($i=0; $i<sizeof($users); $i++) {
 		}
 		echo json_encode($users);

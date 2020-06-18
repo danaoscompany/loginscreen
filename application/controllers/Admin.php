cacaea48025e39419922b01ceb2d58efb4965965
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-06-18 11:18:08 +0700

    Last commit

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -16,23 +16,19 @@ class Admin extends CI_Controller {
 				'super_admin' => 1
 			));
 		} else {
-			echo json_encode(array(
-				'response_code' => -2
-			));
-		}
-		
-		$admins = $this->db->query("SELECT * FROM `admin` WHERE `email`='" . $email . "' AND `password`='" . $password . "'")->result_array();
-		if (sizeof($admins) > 0) {
-			$admin = $admins[0];
-			echo json_encode(array(
-				'response_code' => 1,
-				'user_id' => intval($admin['id']),
-				'super_admin' => 0
-			));
-		} else {
-			echo json_encode(array(
-				'response_code' => -2
-			));
+			$admins = $this->db->query("SELECT * FROM `admin` WHERE `email`='" . $email . "' AND `password`='" . $password . "'")->result_array();
+			if (sizeof($admins) > 0) {
+				$admin = $admins[0];
+				echo json_encode(array(
+					'response_code' => 1,
+					'user_id' => intval($admin['id']),
+					'super_admin' => 0
+				));
+			} else {
+				echo json_encode(array(
+					'response_code' => -2
+				));
+			}
 		}
 	}
 
